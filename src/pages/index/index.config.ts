92e9717fded0183e13c0754848d9e2fd00d823b0
hqchqc <734986933@qq.com>
2021-12-25 22:14:41 +0800

    feat: 完成上半部分，调样式要适配 没想好~~

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,3 +1,3 @@
 export default {
-  navigationBarTitleText: '首页',
+  navigationBarTitleText: '护肤助手',
 };
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,159 +1,36 @@
-import { Calendar, Cell, Field, Popup, Picker, Button } from '@taroify/core';
-import { ArrowRight } from '@taroify/icons';
-import { View } from '@tarojs/components';
-import Taro from '@tarojs/taro';
-import dayjs from 'dayjs';
-import { useEffect, useState } from 'react';
+import { Flex } from '@taroify/core';
 import './index.less';
 
 interface IndexProps {}
 
 const Index: React.FC<IndexProps> = () => {
-  const [minDate] = useState(
-    new Date(new Date().getFullYear(), new Date().getMonth(), 1)
-  );
-  const [maxDate] = useState(
-    new Date(new Date().getFullYear(), new Date().getMonth(), 0)
-  );
-  const [formatValue, setFormatValue] = useState<string>();
-  const [open, setOpen] = useState<boolean>(false);
-  const [value, setValue] = useState<Date>();
-  const [fieldValue, setFieldValue] = useState<string>('');
-  const [openPicker, setOpenPicker] = useState<boolean>(false);
-  const [dayUse, setDayUse] = useState<string[]>([]);
-
-  useEffect(() => {
-    Taro.cloud
-      .callFunction({
-        name: 'dbRun',
-        data: {
-          type: 'get',
-          db: 'dailyRecord',
-          data: {},
-        },
-      })
-      .then(res => {
-        console.log(res);
-        if (res.errMsg === 'cloud.callFunction:ok' && res.result) {
-          setDayUse(res?.result['data']);
-        } else {
-          console.log(res, '210');
-        }
-      });
-  }, []);
-
-  const handleFormatter = (day: Calendar.DayObject) => {
-    const date = day.value.getDate();
-    const month = day.value.getMonth() + 1;
-
-    dayUse.map(item => {
-      const dayItem = item['date'].substring(item['date'].lastIndexOf('-') + 1);
-      if (date === Number(dayItem)) {
-        day.children =
-          item['type'] === 'A' ? (
-            <text className="iconfont">&#xe606;</text>
-          ) : (
-            <text className="iconfont">&#xe608;</text>
-          );
-      }
-    });
-
-    return day;
-  };
-
-  const handleSubmit = (fieldValue: string) => {
-    Taro.cloud
-      .callFunction({
-        name: 'dbRun',
-        data: {
-          type: 'insert',
-          db: 'dailyRecord',
-          data: {
-            type: fieldValue,
-            date: dayjs().format('YYYY-MM-DD'),
-          },
-        },
-      })
-      .then(res => {
-        console.log(res, '--11-1111111--12-');
-      });
-  };
-
   return (
-    <View className="index">
-      <Cell
-        title="瞅瞅今天用啥 👀"
-        clickable
-        rightIcon={<ArrowRight />}
-        children={formatValue}
-        onClick={() => setOpen(true)}
-      />
-      <Popup
-        style={{ height: '80%' }}
-        open={open}
-        rounded
-        placement="bottom"
-        onClose={setOpen}
-      >
-        <Calendar
-          type="single"
-          readonly
-          value={value}
-          min={minDate}
-          max={maxDate}
-          onChange={setValue}
-          formatter={handleFormatter}
-          // onConfirm={newValue => {
-          //   setFormatValue(formatFullDate(newValue));
-          //   setOpen(false);
-          // }}
-        ></Calendar>
-      </Popup>
+    <Flex align="center" direction="column">
+      <Flex>
+        <Flex.Item>您 累 计 喝 掉 8 杯</Flex.Item>
+      </Flex>
 
-      <Field
-        value={fieldValue}
-        label="护肤环节"
-        placeholder="选择护肤"
-        readonly
-        rightIcon={<ArrowRight />}
-        onClick={() => setOpenPicker(true)}
-      />
-      <Popup
-        open={openPicker}
-        rounded
-        placement="bottom"
-        onClose={setOpenPicker}
+      <Flex
+        gutter={20}
+        align="center"
+        justify="space-between"
+        style={{
+          width: '85%',
+          padding: '20px 10px',
+          textAlign: 'center',
+          margin: '15px',
+          // boxShadow: '10px 10px 5px #fff',
+          borderRadius: '10px',
+          border: '1px solid #ccc',
+        }}
       >
-        <Picker
-          onCancel={() => setOpenPicker(false)}
-          onConfirm={values => {
-            setFieldValue(values);
-            setOpenPicker(false);
-          }}
-        >
-          <Picker.Toolbar>
-            <Picker.Button>取消</Picker.Button>
-            <Picker.Title>选择护肤</Picker.Title>
-            <Picker.Button>确认</Picker.Button>
-          </Picker.Toolbar>
-          <Picker.Column>
-            <Picker.Option>A</Picker.Option>
-            <Picker.Option>C</Picker.Option>
-          </Picker.Column>
-        </Picker>
-      </Popup>
-
-      <Button
-        color="info"
-        size="mini"
-        block
-        variant="outlined"
-        style={{ width: '100px', margin: '20px auto' }}
-        onClick={() => handleSubmit(fieldValue[0])}
-      >
-        确认
-      </Button>
-    </View>
+        <Flex.Item span={6}>今年已喝 1 杯</Flex.Item>
+        <Flex.Item span={6} className="monthUse">
+          本月已喝 1 杯
+        </Flex.Item>
+        <Flex.Item span={6}>本周已喝 1 杯</Flex.Item>
+      </Flex>
+    </Flex>
   );
 };
 
