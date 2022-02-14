e98e52b24de0adbb06aac3ca887b723338660442
hqchqc <734986933@qq.com>
2022-02-14 22:35:33 +0800

    feat: 获取用户头像和昵称

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -5,17 +5,10 @@ cloud.init();
 const db = cloud.database();
 
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext();
+  const { OPENID, APPID } = cloud.getWXContext();
 
-  try {
-    return await db.collection('dailyRecord').add({
-      data: {
-        userId: wxContext.OPENID, //获取操作者_openid的方法
-        type: event.type,
-        date: event.date,
-      },
-    });
-  } catch (e) {
-    console.log(e);
-  }
+  return {
+    OPENID,
+    APPID,
+  };
 };
--- src/pages/MyInfo/index.tsx
+++ src/pages/MyInfo/index.tsx
@@ -1,27 +1,28 @@
-import { Image } from '@taroify/core';
+import { Image, Toast } from '@taroify/core';
 import avatarFrameImg from '@assets/img/avatarFrame.svg';
 import avatarImg from '@assets/img/avatar.svg';
 
 import './index.less';
 import Taro from '@tarojs/taro';
+import { useState } from 'react';
 
 interface MyInfoProps {}
 
 const MyInfo: React.FC<MyInfoProps> = () => {
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const [nickName, setNickName] = useState('');
+
   const handleLogin = () => {
-    Taro.login({
-      success(res) {
-        if (res.code) {
-          //发起网络请求
-          Taro.request({
-            url: 'https://example.com/onLogin',
-            data: {
-              code: res.code,
-            },
-          });
-        } else {
-          console.log('登录失败！' + res.errMsg);
-        }
+    Taro.getUserProfile({
+      desc: '用于登录小程序',
+      success: userInfo => {
+        const { avatarUrl, nickName } = userInfo.userInfo;
+        setNickName(nickName);
+        setAvatarUrl(avatarUrl);
+        Toast.success('登录成功！');
+      },
+      fail: () => {
+        Toast.fail('登录失败！请授权~');
       },
     });
   };
@@ -31,12 +32,17 @@ const MyInfo: React.FC<MyInfoProps> = () => {
       <view className="myInfoBg">
         <view className="myInfoBox">
           <Image src={avatarFrameImg} className="avatarFrame" />
-          <Image src={avatarImg} className="avatar" />
+          <Image
+            src={avatarUrl || avatarImg}
+            shape="circle"
+            className="avatar"
+          />
           <text className="loginText" onClick={() => handleLogin()}>
-            点击登录
+            {nickName || '点击登录'}
           </text>
         </view>
       </view>
+      <Toast id="toast" />
     </>
   );
 };
